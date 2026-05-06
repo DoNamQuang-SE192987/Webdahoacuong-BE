9ee99d32c4ee39f945a648889960275c475c7c1e
DoNamQuang-SE192987 <quangdnis@gmail.com>
2026-05-06 17:04:30 +0700

    init project

---
+++ src/main/java/com/se192987/webdahoacuong/WebdahoacuongApplication.java
@@ -0,0 +1,13 @@
+package com.se192987.webdahoacuong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebdahoacuongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebdahoacuongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/se192987/webdahoacuong/WebdahoacuongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se192987.webdahoacuong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebdahoacuongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
